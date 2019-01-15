d47ecfd9eee6e6ff09a8d10a8268a61410a1c2ec
simmoe <simmoe@gmail.com>
2019-01-15 10:59:50 +0100

    Updates

---
+++ precache-manifest.c9936f20a494ba8154e5431514ae70c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b553e34b131052c90b3e",
+    "url": "/react.js/1my-react-app/static/css/main.f4cc3322.chunk.css"
+  },
+  {
+    "revision": "b553e34b131052c90b3e",
+    "url": "/react.js/1my-react-app/static/js/main.b553e34b.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react.js/1my-react-app/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "7cd073a0b85c1dbf8bc5",
+    "url": "/react.js/1my-react-app/static/js/runtime~main.7cd073a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react.js/1my-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "545c22f9b16df89ee77ef1c4ccb0459d",
+    "url": "/react.js/1my-react-app/index.html"
+  }
+];
